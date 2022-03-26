7c6a4649bf683612e29a0e755e3f9b6fb2eecc88
LucasGooes <lucasmarcos343@gmail.com>
2022-03-26 11:40:37 -0400

    Commit Inicial

---
+++ src/main/java/com/agenceteste/emprestcar/EmprestcarApplication.java
@@ -0,0 +1,13 @@
+package com.agenceteste.emprestcar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmprestcarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmprestcarApplication.class, args);
+	}
+
+}
